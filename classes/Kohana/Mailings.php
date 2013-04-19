253b10002f6ba67795e7d7b2882a15bccd2f691a
Alexeyco <6425762@gmail.com>
2013-04-19 21:17:29 +0400

    Mail prepare parsing

--- classes/Kohana/Mailings.php
+++ classes/Kohana/Mailings.php
@@ -40,39 +40,77 @@ abstract class Kohana_Mailings {
 	 * @param  string $css
 	 * @return string
 	 */
-	public static function inline_style($html, $css)
+	public static function inline_style($html, $css_file)
 	{
-		if ( ! class_exists('CSSQuery', FALSE))
+		/**
+		 * This is idiocy, I know, but so far I have no time to write autoload-function...
+		 *
+		 * P.S.
+		 * I am not from India
+		 */
+		$libraries = array
+		(
+			'CSSQuery' => 'CSSQuery/CSSQuery',
+			'InlineStyle' => 'InlineStyle/InlineStyle'
+ 		);
+
+		foreach ($libraries as $class => $path)
 		{
-			require Kohana::find_file('vendor', 'inline-style/CSSQuery');
+			if ( ! class_exists($class, FALSE))
+			{
+				require Kohana::find_file('vendor', $path);
+			}
 		}
 
-		if ( !class_exists('InlineStyle', FALSE))
-		{
-			require Kohana::find_file('vendor', 'inline-style/InlineStyle');
-		}
-
-		$remove_doctype = ! stristr($html, 'DOCTYPE');
+		$curr_dir = pathinfo($css_file, PATHINFO_DIRNAME).DIRECTORY_SEPARATOR;
 
 		$html = iconv(mb_detect_encoding($html), 'UTF-8//TRANSLIT', $html);
 
 		$htmldoc = new InlineStyle($html);
-		$htmldoc->applyStylesheet($css);
+		$htmldoc->applyStylesheet(file_get_contents($css_file));
 
-		$html = $htmldoc->getHTML();
-
-		// Remove DOCTYPE, if it was not in the HTML source
-		if ($remove_doctype)
-		{
-			$html = str_replace(array('<html>', '</html>', '<body>', '</body>'), '', $html);
-			$html = preg_replace('/^<!DOCTYPE.+?>/', '', $html);
-		}
+		$html = self::_process_inline_images($htmldoc->getHTML(), $curr_dir);
 
 		// Then remove unused cass parameters
 		$html = preg_replace('/ class=".*?"/', '', $html);
 		$html = preg_replace('/class=".*?"/', '', $html);
 
-		return InlineCSS::parse($html, $css);
+		return $html;
+	}
+
+	protected static function _process_inline_images($contents, $curr_dir)
+	{
+		preg_match_all('/url\(\s*[\'"]?(\S*\.(?:jpe?g|gif|png))[\'"]?\s*\)/i', $contents, $matches);
+
+		foreach (array_keys($matches[0]) as $i)
+		{
+			$media_file = $curr_dir.DIRECTORY_SEPARATOR.$matches[1][$i];
+
+			$compressed = self::_file_to_data_uri($media_file);
+
+			if ($compressed)
+			{
+				$contents = str_replace($matches[1][$i], $compressed, $contents);
+			}
+		}
+
+		return $contents;
+	}
+
+	/**
+	 * Convert media file to base64 hash
+	 *
+	 * @param   type   $filename
+	 * @return  string
+	 */
+	protected static function _file_to_data_uri($filename)
+	{
+		$extension = pathinfo($filename, PATHINFO_EXTENSION);
+
+		$mime     = File::mime_by_ext($extension);
+		$contents = file_get_contents($filename);
+
+		return 'data:'.$mime.';base64,'.base64_encode($contents);
 	}
 
 } // End Kohana_Mailings
